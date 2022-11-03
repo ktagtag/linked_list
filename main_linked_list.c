3370203b911d771e2dcfb846e8935e92ffda4ec4
ktagtag <98307785+ktagtag@users.noreply.github.com>
2022-11-03 21:33:14 +0900

    Add mainfile

---
+++ main_linked_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linked_list.h"
+
+int main(void) {
+  insert_cell_top(1);
+  insert_cell_top(2);
+  display();
+  insert_cell(head, 3);
+  insert_cell(head, 4);
+  display();
+
+  delete_cell(head);
+  display();
+
+  delete_cell_top();
+  display();
+
+  return EXIT_SUCCESS;
+}
